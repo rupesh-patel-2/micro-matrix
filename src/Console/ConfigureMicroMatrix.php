12036d2e62c4cf32093b76c0b3d50936b28b553b
Rupesh Patel <rupesh.patel@matrixhive.com>
2023-04-05 14:21:54 +0530

    Configure command for Micro Matrix

--- src/Console/ConfigureMicroMatrix.php
+++ src/Console/ConfigureMicroMatrix.php
@@ -13,5 +13,27 @@ class ConfigureMicroMatrix extends Command
     public function handle(){
         $this->info('Configuring MicroMatrix...');
 
+        $this->info('Running Migrations for base tables ...');
+
+        $migrationsPath = __DIR__ . '/../database/migrations';
+        $destMigrationDir = base_path('database').'/micro-matrix';
+
+        if( !is_dir($destMigrationDir) ){
+            mkdir( $destMigrationDir );
+        }
+
+        $files = scandir($migrationsPath);
+        foreach($files as $file){
+            $sourceFile = $migrationsPath.'/'.$file;
+            if ( is_file($sourceFile) ){
+                $destFile = $destMigrationDir.'/'.$file;
+                if( !is_file($destFile) ) {
+                    copy( $sourceFile , $destFile);
+                }
+            }
+        }
+        
+        $res = \Artisan::call('migrate --path="database/micro-matrix"');
+        $this->info( ( string ) \Artisan::output() );
     }
 }
--- src/MicroMatrixServiceProvider.php
+++ src/MicroMatrixServiceProvider.php
@@ -1,13 +1,19 @@
 <?php
 namespace Rupesh\MicroMatrix;
-
+use Rupesh\MicroMatrix\Console\ConfigureMicroMatrix;
 use Illuminate\Support\ServiceProvider;
 
 class MicroMatrixServiceProvider extends ServiceProvider {
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        // $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ConfigureMicroMatrix::class,
+            ]);
+        }
+        //$this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         // Manager::initiateTenant(1);
     }
     public function register()
